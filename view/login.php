bd8d0b6d3701a9ed0289ceec1da2684fd4cbaa98
Mathieu LUCAS <mathieu.lucas.2956@gmail.com>
2015-02-28 20:13:31 +0100

    login dans un panel (juste cosmetique)

--- view/login.php
+++ view/login.php
@@ -1,11 +1,18 @@
 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2">
-	<form id="loginForm" role="form" action="administrator" method="post">
-		<div class="form-group">
-			<input type="text" class="form-control" name="login" placeholder="login"/>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h3 class="panel-title">Connexion</h3>
 		</div>
-		<div class="form-group">
-			<input type="password" class="form-control" name="password" placeholder="mot de passe" />
+		<div class="panel-body">
+			<form id="loginForm" role="form" action="administrator" method="post">
+				<div class="form-group">
+					<input type="text" class="form-control" name="login" placeholder="login"/>
+				</div>
+				<div class="form-group">
+					<input type="password" class="form-control" name="password" placeholder="mot de passe" />
+				</div>
+				<button type="submit" class="btn btn-primary">Connexion</button>
+			</form>
 		</div>
-		<button type="submit" class="btn btn-primary">Connexion</button>
-	</form>
+	</div>
 </div>
