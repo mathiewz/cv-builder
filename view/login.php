d64bd9c32cfaa9129073729d1904fb8e82f0f646
Mathieu LUCAS <mathieu.lucas.2956@gmail.com>
2015-02-28 23:10:35 +0100

    centrage de la modal de login + changement de la couleur de fond

--- view/login.php
+++ view/login.php
@@ -1,10 +1,10 @@
-<div class="col-xs-12 col-sm-6 col-md-4 col-lg-2">
+<div id="loginForm" class="col-xs-12 col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4 col-lg-offset-5 col-lg-2">
 	<div class="panel panel-default">
 		<div class="panel-heading">
 			<h3 class="panel-title">Connexion</h3>
 		</div>
 		<div class="panel-body">
-			<form id="loginForm" role="form" action="administrator" method="post">
+			<form role="form" action="administrator" method="post">
 				<div class="form-group">
 					<input type="text" class="form-control" name="login" placeholder="login"/>
 				</div>
