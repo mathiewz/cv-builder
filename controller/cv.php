1d9abdb678ed178b602e162eec9a202702e62e2d
mathiewz <contact@mathieu-lucas.fr>
2015-03-03 08:45:08 +0100

    correction de la déconnexion

--- controller/cv.php
+++ controller/cv.php
@@ -1,7 +1,10 @@
 <?php
-	if(isset($_POST['deco']) && $_POST['deco']){
-		session_start ();
-		unset($_SESSION['connected']);
+	session_start ();
+	if(isset($_SESSION['connected'])){	//Si on est connecté, on vérifie est ceque l'on a demandé la déconnexion, et si c'est le cas on vide la variable de session
+		if(isset($_POST['deco']) && $_POST['deco']){
+			unset($_SESSION['connected']);
+			header('location: .');	//on recharge la page pour eviter que l'appui sur F5 propose de renvoyer le formulaire une nouvelle fois
+		}
 	}
 	$views = array();
 	$views[] = 'cv';
