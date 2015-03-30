62b65dce227c181daaa2397d0bce8f288f9d8816
MathieuLucas <contact@mathieu-lucas.fr>
2015-03-30 12:08:00 -0400

    Suppression d'un test qui trainait

--- model/presentation.php
+++ model/presentation.php
@@ -4,7 +4,6 @@
     function updateDesc($nom, $prenom, $birthday, $description){
         $pdo = PdoSio::getPdoSio();
         $request = "REPLACE into description (id, nom, prenom, birthday, description) values(1, '".$nom."', '".$prenom."', '".$birthday."', '".$description."');";
-        echo $request;
         $res = $pdo->actionRequest($request);
     }
 
