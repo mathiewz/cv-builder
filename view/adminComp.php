c4ee330d1038ea3989104ffd0c4df87fd68baaf1
Mathieu LUCAS <mathieu.lucas.2956@gmail.com>
2015-04-01 00:58:48 +0200

    correction du color picker sur iPhone

--- view/adminComp.php
+++ view/adminComp.php
@@ -50,10 +50,6 @@
 <div class="modal fade color-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-sm">
         <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Séléctionner une couleur</h4>
-            </div>
             <div class="modal-body">
                 <div id="colorpicker"></div>
             </div>
