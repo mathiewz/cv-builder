439a717e0413ee01d51dc34cf5f5019cad917807
MathieuLucas <contact@mathieu-lucas.fr>
2015-04-10 07:44:24 -0400

    ajout d'un number picker pour bootstrap

--- view/adminComp.php
+++ view/adminComp.php
@@ -8,20 +8,20 @@
             <h2 class="panel-title">Ajoutez une catégorie</h2>
         </div>
         <div class="panel-body">
+            <input type="hidden" name="formSend" value="addCategorie"/>
             <form action="admin-competences" method="post">
-                <input type="hidden" name="formSend" value="addCategorie"/>
                 <div class="form-group col-xs-12">
                     <div class="col-xs-12 col-md-6">
-                        <div class="form-group input-group group-prez col-xs-12">
+                        <div class="form-group input-group col-xs-12">
                             <div class="hidden-xs input-group-addon">Nom</div>
                             <label for="nom" class="visible-xs">Nom</label>
                             <input type="text" class="form-control" name="nom" id="nom" required />
                         </div>
                     </div>
                     <div class="col-xs-12 col-md-6">
-                        <input type="hidden" class="form-control" name="color" id="color" value="#428bca" required />
                         <a class="btn btn-primary col-xs-12 col-sm-4" data-toggle="modal" data-target=".color-modal-sm">Couleur</a>
                         <div class="col-xs-12 col-sm-8">
+                            <input type="hidden" class="form-control" name="color" id="color" value="#428bca" required />
                             <div id="colorDemoProgress" class="progress">
                                 <div class="progress-bar progress-bar-striped active fb-callback" style="width: 100%">
                                     <!--La progressBar est générée ici par Bootstrap ! -->
@@ -45,19 +45,44 @@
         <div class="panel-body">
             <form action="admin-competences" method="post">
                 <input type="hidden" name="formSend" value="addCompetence"/>
-                <!-- Ce select contient toutes les catégories rentrées plus tot -->
-                <div class="hidden-xs input-group-addon">Nom</div>
-                <label for="nom" class="visible-xs">Nom</label>
-                <input type="text" class="form-control" name="nom" id="nom" required />
+                <div class="col-xs-12 col-sm-6">
+                    <div class="form-group input-group col-xs-12">
+                        <div class="hidden-xs input-group-addon">Nom</div>
+                        <label for="nom" class="visible-xs">Nom</label>
+                        <input type="text" class="form-control" name="nom" id="nom" required />
+                    </div>
+                </div>
                 
-                <select name="categorie" class="form-control">
-                    <?php
-                    foreach($categories as $id => $name){
-                        echo '<option value="'.$id.'">'.$name.'</option> ';
-                    }
-
-                    ?>
-                </select>
+                <!-- Ce select contient toutes les catégories rentrées plus tot -->
+                <div class="col-xs-12 col-sm-6">
+                    <div class="form-group input-group col-xs-12">
+                        <div class="hidden-xs input-group-addon">Catégorie</div>
+                        <label for="categorie" class="visible-xs">Catégorie</label>
+                        <select name="categorie" class="form-control">
+                            <?php
+                            foreach($categories as $id => $name){
+                                echo '<option value="'.$id.'">'.$name.'</option> ';
+                            }
+                            ?>
+                        </select>
+                    </div>
+                </div>
+                
+                <div class="col-xs-12 col-sm-6">
+                    <div class="form-group input-group col-xs-12">
+                        <div class="hidden-xs input-group-addon">Nom</div>
+                        <label for="nom" class="visible-xs">Nom</label>
+                        <input type="text" class="form-control" name="nom" id="nom" required />
+                    </div>
+                </div>
+                
+                <div class="col-xs-12 col-sm-6">
+                    <div class="form-group input-group col-xs-12">
+                        <div class="hidden-xs input-group-addon">Niveau (en %)</div>
+                        <label for="niveau" class="visible-xs">Niveau (en %)</label>
+                        <input id="niveau" type="text" value="55" name="niveau">
+                    </div>
+                </div>
                 <div id="addCatButton" class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                     <input type="submit" class="btn btn-primary col-xs-12" value="Ajouter" />
                 </div>
@@ -75,7 +100,6 @@
     <li>parcours pro</li>
     <li>Compétences
         <ul>
-            <li>Catégories</li>
             <li>Compétences</li>
         </ul>
     </li>
@@ -97,7 +121,14 @@
 </div>
 
 <script type="text/javascript">
-  $(document).ready(function() {
-    $('#colorpicker').farbtastic('#color');
-  });
+    $(document).ready(function() {
+        $('#colorpicker').farbtastic('#color');
+    });
+
+    $("#niveau").TouchSpin({
+        min: 0,
+        max: 100,
+        boostat: 5,
+        maxboostedstep: 10
+    });
 </script>
